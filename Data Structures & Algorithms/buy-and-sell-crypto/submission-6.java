739ede3981de5d488f8922eaa5a3f1e0aa707dfa
Hamza Dadda <109368243+Marabii@users.noreply.github.com>
2026-09-13 21:07:56 +0200

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        int left = 0;
+        int right = 0;
+
+        while (right < prices.length) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            } else {
+                profit = Math.max(profit, prices[right] - prices[left]);
+            }
+            right++;
+        }
+
+        return profit;
+    }
+}
